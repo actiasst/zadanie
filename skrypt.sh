8e7c8e4c214a0ba1778585545e226025d171a63c
actiasst <maciekactias@gmail.com>
2016-05-31 20:36:43 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+d=`date +%u`
+
+if [ $d == 5 ] || [ $d == 6 ] || [ $d == 7 ]
+
+then
+
 a=`./p2.out`
 
 b=`echo $a | aspell -a | wc -w`
 
 if [ $b == 10 ]
+
 then
+
 echo "Wyraz wystepuje w jezyku polskim"
+
 else
+
 echo "Wyraz nie wystepuje w jezyku polskim lub jest blednie napisany"
+
+fi
+
 fi
